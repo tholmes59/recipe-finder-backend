e72c34e245e02e9152aa7ececaefcbf37cadc449
Thomas <tholmes59@gmail.com>
2019-10-03 15:39:12 -0700

    add namespace to routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :recipes
-  resources :favorites
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :recipes
+      resources :favorites
+    end
+  end
+  
 end
