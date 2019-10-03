64c5ded78c8992e48ebb1802f7184e6fe54dbbad
Thomas <tholmes59@gmail.com>
2019-10-03 15:09:06 -0700

    changed name of item in Recipe table

--- db/migrate/20191003214256_create_recipes.rb
+++ db/migrate/20191003214256_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[5.2]
   def change
     create_table :recipes do |t|
       t.string :recipe_title
-      t.string :recipe_id
+      t.string :recipe_identifier
 
       t.timestamps
     end
