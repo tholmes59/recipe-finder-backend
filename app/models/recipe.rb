1bdc42b41d23be106c02aaaa84a39d22398a7d64
Thomas <tholmes59@gmail.com>
2019-10-04 09:44:47 -0700

    create models and assosciations for user, recipe and favorite

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+    has_many :favorites
+    has_many :users, through: :favorites 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    has_many :favorites
+    has_many :recipes, through: :favorites
+
+    validates :name, :username, :email, :password, :presense => true
+    validates :username, :uniqueness => true
+
 end
