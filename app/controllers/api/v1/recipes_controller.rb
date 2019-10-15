05b2249712d1cb57044c1da823b19beb99b17d08
Thomas <tholmes59@gmail.com>
2019-10-15 11:09:16 -0700

    began building recipes controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::RecipesController < ApplicationController
+    before_action :set_recipe, only: [:show, :update, :destroy]
+
+    def index
+        @recipes = Recipe.all 
+
+        render json: @recipes 
+    end
+
+    def show
+        render json: @recipe 
+    end
+
+    def create
+
+    end
+
+
+    def update
+
+    end
+
+    def destroy
+        @recipe.destroy
+    end
+
+    private
+
+    def set_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:title, :recipe_identifier)
+    end
+
 end
