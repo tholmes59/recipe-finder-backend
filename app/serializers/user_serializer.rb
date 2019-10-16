bcc20e89a4cbbe9668ef776dd5ddb883c76fa58a
Thomas <tholmes59@gmail.com>
2019-10-16 08:49:23 -0700

    edit user serializer to include favorites

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,11 @@ class UserSerializer
     include FastJsonapi::ObjectSerializer
     
     attributes :username, :name, :email
+
+    has_many :favorites, serializer: FavoritesSerializer
+
+    attribute :favorites do |object|
+      FavoritesSerializer.new(object.favorites).as_json["data"]
+    end
     
   end
