8595661c6e9ed14272901d62c3a65a5b15238e39
Thomas <tholmes59@gmail.com>
2019-10-16 08:50:58 -0700

    begin building favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::FavoritesController < ApplicationController
+
+    def index
+        if logged_in?
+            @favorites = current_user.favorites
+            render json: FavoritesSerializer.new(@favorites)
+        else
+            render json: { error: "Please log in to see your favorites"}
+        end
+    end
+
+    def show
+        
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        @favorite.destroy
+    end
+
+
 end
