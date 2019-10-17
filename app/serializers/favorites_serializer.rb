ac6954b0c2368b3efa9cf544a8bfbee23b82621f
Thomas <tholmes59@gmail.com>
2019-10-17 15:59:06 -0700

    fix typo on recipe_title attribute and add recipe_ifentifier attribute to favorites serializer

--- app/serializers/favorites_serializer.rb
+++ app/serializers/favorites_serializer.rb
@@ -1,7 +1,11 @@
 class FavoritesSerializer
     include FastJsonapi::ObjectSerializer
 
-    attribute :title do |object|
-        object.recipe.title 
+    attribute :recipe_title do |object|
+        object.recipe.recipe_title 
+    end
+
+    attribute :recipe_identifier do |object|
+        object.recipe.recipe_identifier
     end
 end
