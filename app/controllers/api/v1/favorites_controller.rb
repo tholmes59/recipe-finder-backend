b63db33f250916d4bfd3a6e69767df33dd614ae6
Thomas <tholmes59@gmail.com>
2019-10-16 17:11:43 -0700

    continue to build favorites controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::FavoritesController < ApplicationController
+    before_action :set_favorite, only: [:show, :update, :destroy]
 
     def index
         if logged_in?
@@ -10,20 +11,37 @@ class Api::V1::FavoritesController < ApplicationController
     end
 
     def show
-        
+        render json: FavoritesSerializer.new(@favorite)
     end
 
     def create
-
+        @favorite = Favorite.new(favorite_params)
+        if @favorite.save 
+            render json: @favorite, status: :created
+        else
+            render json: @favorite.errors, status: :unprocessable_entity
+        end
     end
 
     def update
-
+        if @favorite.update(favorite_params)
+            render json: @favorite
+        else
+            render json: @favorite.errors, status: :unprocessable_entity
+        end
     end
 
     def destroy
         @favorite.destroy
     end
 
+    private
 
+    def set_favorite
+        @favorite = Favorite.find(params[:id])
+    end
+
+    def favorite_params
+        params.require(:favorite).permit(:user_id, :favorite_id)
+    end
 end
