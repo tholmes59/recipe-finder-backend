a7662f2d9d285d98e9dfe3256132880e35b5a343
Thomas <tholmes59@gmail.com>
2019-10-23 10:50:09 -0700

    add recipe image to saved recipes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -58,7 +58,7 @@ class Api::V1::RecipesController < ApplicationController
     end
 
     def recipe_params
-        params.require(:recipe).permit(:recipe_title, :recipe_identifier)
+        params.require(:recipe).permit(:recipe_title, :recipe_identifier, :recipe_image)
     end
 
 end
